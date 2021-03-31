1f681b5a224174f542600fcb32c8e618ffeac475
Emilio Buchaillot <emilio.buchaillot@semperti.com>
2021-03-31 16:43:20 -0300

    j

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -96,9 +96,10 @@ pipeline {
                     script {
                         files = findFiles(glob: 'build/**')
                         files.each { 
-                            def deletedBuild = it.replaceAll("build/","")
-                            println "File:  ${it}"
-                            s3Upload(file:"${it}", bucket:"${s3Artifact}",path:"${it}")
+                            def it2 = it
+                            it.replaceAll("build/","")
+                            println "File:  ${deletedBuild}"
+                            s3Upload(file:"${it2}", bucket:"${s3Artifact}",path:"${deletedBuild}")
                         }
                     }
                 }
