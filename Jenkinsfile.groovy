7c678a0119c8e0fe19790672a69e4f9b4f02a2ba
Emilio Buchaillot <emilio.buchaillot@semperti.com>
2021-04-07 17:26:10 -0300

    Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,28 +1,40 @@
 @Library("shared-library") _
-reactBuildPipeline{
-    npmCmd      = "npm"
-    devTag      = "0.0-0"
-    prodTag     = "0.0"
-    prefix = "semperti"
-    app = "react"
-    env = "development-rapientrega"
-    artifact = "${app}.zip"
-    s3Artifact = "${prefix}-${app}-${env}"
-    awsCredentials = "aws-bucha"
-    awsRegion = "us-east-1"
-    awsProfile = "semperti"
-}
-@Library("shared-library") _
-reactDeployS3Pipeline{
-    npmCmd      = "npm"
-    devTag      = "0.0-0"
-    prodTag     = "0.0"
-    prefix = "semperti"
-    app = "react"
-    env = "development-rapientrega"
-    artifact = "${app}.zip"
-    s3Artifact = "${prefix}-${app}-${env}"
-    awsCredentials = "aws-bucha"
-    awsRegion = "us-east-1"
-    awsProfile = "semperti"
+pipeline
+{
+    agent any
+
+    stages {
+        stage("build"){
+            steps{
+             reactBuildPipeline{
+                npmCmd      = "npm"
+                devTag      = "0.0-0"
+                prodTag     = "0.0"
+                prefix = "semperti"
+                app = "react"
+                env = "development-rapientrega"
+                artifact = "${app}.zip"
+                s3Artifact = "${prefix}-${app}-${env}"
+                awsCredentials = "aws-bucha"
+                awsRegion = "us-east-1"
+                awsProfile = "semperti"
+            }}
+        }
+        stage("deploy"){
+             steps{
+            reactDeployS3Pipeline{
+                npmCmd      = "npm"
+                devTag      = "0.0-0"
+                prodTag     = "0.0"
+                prefix = "semperti"
+                app = "react"
+                env = "development-rapientrega"
+                artifact = "${app}.zip"
+                s3Artifact = "${prefix}-${app}-${env}"
+                awsCredentials = "aws-bucha"
+                awsRegion = "us-east-1"
+                awsProfile = "semperti"
+            } }
+        }
+    }
 }
