a009a130bfa823241241ab0e681733ae204d430f
Emilio Buchaillot <emilio.buchaillot@semperti.com>
2021-04-07 17:29:05 -0300

    Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,40 +1,14 @@
 @Library("shared-library") _
-pipeline
-{
-    agent any
-
-    stages {
-        stage("build"){
-            steps{
-             reactBuildPipeline{
-                npmCmd      = "npm"
-                devTag      = "0.0-0"
-                prodTag     = "0.0"
-                prefix = "semperti"
-                app = "react"
-                env = "development-rapientrega"
-                artifact = "${app}.zip"
-                s3Artifact = "${prefix}-${app}-${env}"
-                awsCredentials = "aws-bucha"
-                awsRegion = "us-east-1"
-                awsProfile = "semperti"
-            }}
-        }
-        stage("deploy"){
-             steps{
-            reactDeployS3Pipeline{
-                npmCmd      = "npm"
-                devTag      = "0.0-0"
-                prodTag     = "0.0"
-                prefix = "semperti"
-                app = "react"
-                env = "development-rapientrega"
-                artifact = "${app}.zip"
-                s3Artifact = "${prefix}-${app}-${env}"
-                awsCredentials = "aws-bucha"
-                awsRegion = "us-east-1"
-                awsProfile = "semperti"
-            } }
-        }
-    }
+reactAwsS3Pipeline{
+    npmCmd      = "npm"
+    devTag      = "0.0-0"
+    prodTag     = "0.0"
+    prefix = "semperti"
+    app = "react"
+    env = "development-rapientrega"
+    artifact = "${app}.zip"
+    s3Artifact = "${prefix}-${app}-${env}"
+    awsCredentials = "aws-bucha"
+    awsRegion = "us-east-1"
+    awsProfile = "semperti"
 }
