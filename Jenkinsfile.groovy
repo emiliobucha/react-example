4e11ac7d638d84a8d1c6dfee6f5b929bb628c132
Emilio Buchaillot <emilio.buchaillot@semperti.com>
2021-04-07 17:16:10 -0300

    Jenkinsfile

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -12,3 +12,16 @@ reactBuildPipeline{
     awsRegion = "us-east-1"
     awsProfile = "semperti"
 }
+reactDeployS3Pipeline{
+    npmCmd      = "npm"
+    devTag      = "0.0-0"
+    prodTag     = "0.0"
+    prefix = "semperti"
+    app = "react"
+    env = "development-rapientrega"
+    artifact = "${app}.zip"
+    s3Artifact = "${prefix}-${app}-${env}"
+    awsCredentials = "aws-bucha"
+    awsRegion = "us-east-1"
+    awsProfile = "semperti"
+}
