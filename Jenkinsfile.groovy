6b49a544051f1297fd74b3db9807d5cbd6b72fa5
Emilio Buchaillot <emilio.buchaillot@semperti.com>
2021-04-13 09:50:05 -0300

    Corrección de texto

--- Jenkinsfile.groovy
+++ Jenkinsfile.groovy
@@ -1,8 +1,8 @@
 @Library("shared-library") _
-reactAwsS3Pipeline{
+reactAwsS3StandardPipeline{
     NPM_CMD      = "npm"
     S3_BUCKET = "semperti-react-development-rapientrega"
-    AWS_CREDENTIALS = "aws-bucha"
     AWS_REGION = "us-east-1"
     BUILD_FOLDER = "build"
+    DEPLOY_JOB = "RapientregaFrontend-Deploy"
 }
